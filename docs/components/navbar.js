884253c24f844f2ca608fa00167a53692a8db343
Riley <fanusr@vcu.edu>
2025-07-12 13:09:44 -0400

    removed leading slash on navbar brand homepage href

--- docs/components/navbar.js
+++ docs/components/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
       <nav class="navbar navbar-expand-md bg-dark border-bottom border-body sticky-top" data-bs-theme="dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="/index.html"><i class="bi bi-suit-heart h3"> VA Polyamory</i></a>
+          <a class="navbar-brand" href="index.html"><i class="bi bi-suit-heart h3"> VA Polyamory</i></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
