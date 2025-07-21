3093239855395a20b3cfe882369edf6f9665fc3b
Riley <fanusr@vcu.edu>
2025-07-21 18:43:46 -0400

    made script to resize librarything iframe

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,11 @@
 // This is where custom js will go
+
+window.addEventListener('message', (event) => {
+  // Verify the message type to avoid unwanted messages
+  if (event.data?.type === 'librarythingHeight') {
+    const iframe = document.querySelector('iframe[src="librarything-widget.html"]');
+    if (iframe && event.data.height) {
+      iframe.style.height = event.data.height + 'px';
+    }
+  }
+});
